8829b7c0b067ae69d2fb85b6f0dd024d4eebaf04
Luis Montoya <skyfire5990@gmail.com>
2026-04-27 20:47:08 -0600

    login

---
+++ app/src/main/java/com/example/myloginjpc/ui/theme/login/ui/LoginScreen.kt
@@ -0,0 +1,133 @@
+package com.example.myloginjpc.ui.theme.login.ui
+
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myloginjpc.R
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoginScreen() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        Login(Modifier.align(Alignment.TopCenter))
+    }
+}
+
+@Composable
+fun Login(modifier: Modifier) {
+    Column(modifier = modifier) {
+        HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier.padding(16.dp))
+        EmailField()
+        Spacer(Modifier.padding(16.dp))
+        PasswordField()
+        Spacer(Modifier.padding(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+        Spacer(Modifier.padding(16.dp))
+        LoginButton()
+    }
+}
+
+@Composable
+fun LoginButton() {
+    Button(
+        onClick = {}, modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFF4303),
+            disabledContainerColor = Color(0xFFFF78058),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        )
+    ) {
+        Text(
+            text = "Iniciar sesión"
+        )
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "¿Olvidaste la contraseña?",
+        modifier = modifier.clickable {},
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red
+    )
+}
+
+@Composable
+fun PasswordField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = {
+            Text(
+                text = "Ingrese contraseña"
+            )
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(Color.Blue, focusedPlaceholderColor = Color.DarkGray)
+    )
+}
+
+@Composable
+fun EmailField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = {
+            Text(
+                text = "Ingrese email"
+            )
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(Color.Blue, focusedPlaceholderColor = Color.DarkGray)
+    )
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.loginimg),
+        contentDescription = "Login_img",
+        modifier = modifier
+    )
+}
